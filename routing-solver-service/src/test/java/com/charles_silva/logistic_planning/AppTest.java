e353fc70a88106e576e9787089f6700a11e60f91
Charles Silva <charles.cassio.silva@gmail.com>
2021-01-13 08:38:29 -0500

    creating solver sample

---
+++ routing-solver-service/src/test/java/com/charles_silva/logistic_planning/AppTest.java
@@ -0,0 +1,14 @@
+package com.charles_silva.logistic_planning;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest 
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
