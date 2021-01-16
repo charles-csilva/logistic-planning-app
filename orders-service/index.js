353ca36aa001a0e90fa09bdeec46278bb82bfe7d
Charles Silva <charles.cassio.silva@gmail.com>
2021-01-16 16:12:00 -0500

    introducing api gateway

--- orders-service/index.js
+++ orders-service/index.js
@@ -1,24 +1,7 @@
 const { ServiceBroker } = require("moleculer");
-const ApiGatewayService = require("moleculer-web");
 
 const broker = new ServiceBroker({
   transporter: "redis://redis-service:6379",
 });
-broker.createService({
-  mixins: [ApiGatewayService],
-  settings: {
-    port: 9000,
-    routes: [
-      {
-        path: "/api",
-      },
-    ],
-    cors: {
-      methods: ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"],
-      origin: "*",
-    },
-  },
-});
-
 broker.loadServices("./");
 broker.start();
