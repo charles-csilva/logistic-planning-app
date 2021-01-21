f25719e597e6cc1125d0e12febd3356020446a5a
Charles Silva <charles.cassio.silva@gmail.com>
2021-01-21 01:19:07 -0500

    configuring dev execution

--- run-dev.sh
+++ run-dev.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 bash run-extract-map-data.sh
 docker build -t directions-service ./directions-service/
-docker build -t routing-solver-service ./routing-solver-service/
 npx concurrently \
 "yarn --cwd ./front-end start" \
 "docker run -p 6379:6379 redis:rc-alpine3.12" \
@@ -9,4 +8,4 @@ npx concurrently \
 "yarn --cwd ./orders-service start" \
 "yarn --cwd ./shipment-service start" \
 "docker run -p 8989:8989 -v $PWD/directions-service/data/:/data directions-service /data/toronto.xml" \
-"docker run -p 8080:8080 routing-solver-service"
+"mvn -f routing-solver-service/pom.xml spring-boot:run"
